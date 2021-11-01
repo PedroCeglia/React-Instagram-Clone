b7a3056cd0342bb4ceaa6eb0082270e1e692bf84
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-01 18:39:44 -0300

    Add Intro Page

---
+++ src/Pages/Intro/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+export default function Intro(){
+    return(
+        <div className='container-intro'>
+            <h2>Intro</h2>
+        </div>
+    )
+}
