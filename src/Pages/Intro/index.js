4590d678d45df97637fb2a7dd193ca5555820a1b
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-02 18:39:54 -0300

    Finish Page Intro

--- src/Pages/Intro/index.js
+++ src/Pages/Intro/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './style.css'
 
+// Import History
+import { useHistory } from 'react-router'
+
+// Import VerifyAuth
+import { verifyUserIsLog } from '../../Firebase/ApiAuth'
+
 export default function Intro(){
+    const history = useHistory()
+    function setPage(){
+        if(verifyUserIsLog() == "true"){
+            history.push('/home')
+        }else{
+            history.push('/enter')
+        }
+    }
+    useEffect(()=>{
+        setTimeout(setPage, 3000)
+    })
     return(
         <div className='container-intro'>
-            <h2>Intro</h2>
+            <img src='assets/logo-black-white.png'/>
         </div>
     )
 }
