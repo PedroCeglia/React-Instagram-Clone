71394081054d5f0d14b0a2fa817467c5fd428faa
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-02 17:25:28 -0300

    Finish to create Route and PrivateRoute

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -1,41 +1,40 @@
 import React, {useState, useEffect} from "react";
 // import react-router-dom
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+// Import Auth
+import {auth} from "../Firebase/FirebaseConfig"
 // import verifyAuth
-import { verifyUserIsLog } from "../Firebase/ApiAuth";
+import {onAuthStateChanged } from "@firebase/auth";
+
+import {verifyUserIsLog} from "../Firebase/ApiAuth"
 // import Pages
 import Home from "../Pages/Home";
 import Intro from "../Pages/Intro";
+import Enter from "../Pages/Enter";
 
 
 
-function PrivateRoute({component: Component, isLog, log, pathRedirect, ...rest}){
+function PrivateRoute({component: Component, log, pathRedirect, ...rest}){
     return(
         <Route
             {...rest}
-            render={props => 
-                    
-                (isLog == log)?(
+            render={props =>
+                (verifyUserIsLog() == log)?(
                     <Component {...props}/>
                     ):(
                     <Redirect to={{pathname: pathRedirect, state:{from: props.location}}}/>
-                    )
-                
+                )
             }
         />  
     )
 }
 export default function Rotas(){
-    const [isLog, setIsLog] = useState(true)
-    useEffect(()=>{
-        verifyUserIsLog(setIsLog)
-    },[])
-    
     return(
         <BrowserRouter>
             <Switch>
-                <PrivateRoute exact path='/' isLog={isLog} log={false} component={Intro} pathRedirect='/home'/>
-                <PrivateRoute path='/home' isLog={isLog} log={true} component={Home} pathRedirect='/'/>
+                <Route exact path='/' component={Intro}/>
+                <PrivateRoute exact path='/enter'  log={"false"} component={Enter} pathRedirect='/'/>
+                <PrivateRoute exact path='/home'  log={"true"} component={Home} pathRedirect='/'/>
             </Switch>
         </BrowserRouter>
     )
