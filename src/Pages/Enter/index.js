c9c1be50c741a9c1e85792b2c440a708e18c3cd7
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-02 17:24:30 -0300

    Add LogIn And SingIn Page

---
+++ src/Pages/Enter/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+export default function Enter(){
+    return(
+        <div className='container-enter'>
+            <h2>Enter</h2>
+        </div>
+    )
+}
