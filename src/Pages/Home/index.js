38de9a1078a57f063bdd7ae2dd10450f02a4ea97
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-03 09:46:52 -0300

    Add LogOut Function

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './style.css'
 
+// import ApiAuth
+import { logOutUser } from '../../Firebase/ApiAuth'
+
 export default function Home(){
     return(
         <div className='container-home'>
             <h2>Home</h2>
+            <button onClick={logOutUser}> west cost bitch</button>
         </div>
     )
 }
