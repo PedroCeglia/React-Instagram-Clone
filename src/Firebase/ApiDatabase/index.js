6e8532d7346ce0588ba8146e029b107ae6aa1dce
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-03 17:13:22 -0300

    Add CreateUserInDatabase and SetUserNameInDatabase

---
+++ src/Firebase/ApiDatabase/index.js
@@ -0,0 +1,24 @@
+// Import getDatabase
+import { database } from "../FirebaseConfig";
+// Import Database Functions
+import {set, ref, update} from '@firebase/database'
+
+// Create User In Database
+export function createUserInDatabase(email, id){
+    const userRef = ref(database, `usuarios/${id}`)
+    set(userRef,{
+        email:email,
+        id:id,
+        publicacoes:0,
+        seguidores:0,
+        seguindo:0
+    })
+}
+
+// Update User Name In Database 
+export function setUserNameDatabase(name, id){
+    const userRef = ref(database, `usuarios/${id}`)
+    update(userRef, {
+        nome:name
+    })
+}
