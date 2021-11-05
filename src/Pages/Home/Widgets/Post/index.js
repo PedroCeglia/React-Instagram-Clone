ec428e60b86eeb3d8ba6d97755f086b5a18ad69c
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-05 00:11:40 -0300

    Add Header

--- src/Pages/Home/Widgets/Post/index.js
+++ src/Pages/Home/Widgets/Post/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './style.css'
 
+// Import Router Lib
+import { useLocation, useHistory } from 'react-router'
+
+// Import AuthApi
+import { VerifyListenerUserIsLog } from '../../../../Firebase/ApiAuth'
+
+// Import Widgets
+import Header from '../Header'
+
 export default function Post(){
+    
+    // Listener IsLog
+    const isLog = VerifyListenerUserIsLog()
+
+    // Change To Intro Page
+    const history = useHistory()
+    useEffect(()=>{
+        if(isLog === "false"){
+            history.push('/')
+        }
+    },[isLog, history])
+
+    // Set Image Resource
+    const pathName = useLocation().pathname
+
     return(
         <div>
+            <Header
+                pathname={pathName}
+            />
             Post
         </div>
     )
