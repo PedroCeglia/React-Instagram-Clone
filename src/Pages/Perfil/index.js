5771a38582fc3130fcc5c24affad2fe7346ba669
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-07 13:40:18 -0300

    Add UI Perfil Page

--- src/Pages/Perfil/index.js
+++ src/Pages/Perfil/index.js
@@ -26,12 +26,47 @@ export default function Perfil(){
     // Set Image Resource
     const pathName = useLocation().pathname
 
+    const descricao = `Página dedicada a expandir a cultura do surf 🌊🏄‍♂️ \n
+                        🌊🌊—-picos da zona sul nos stories—-🏄‍♂️ \n
+                        Procurando equipamentos de surf \n
+                        Nossa loja @surfshopbrasil`
+
     return(
         <div>
             <Header
                 pathname={pathName}
             />
-            Perfil
+            <div className='perfil-container'>
+                <div className='perfil-info'>
+                    <img src='../assets/perfil.png' alt='User Perfil' title='Alterar foto de perfil'/>
+                    <div className='text-info'>
+                        <div>
+                            <span className='user-name'>user_name</span>
+                            <button>Editar Perfil</button>
+                        </div>
+                        <div className='perfil-number'>
+                            <span className='publicacoes'><strong>0</strong> Publicações</span>
+                            <span className='seguidores'><strong>0</strong> Seguidores</span>
+                            <span className='seguindo'><strong>0</strong> Seguindo</span>
+                        </div>
+                        <p className='description'>
+                            {descricao}
+                        </p>
+                    </div>
+                </div>
+                <div className='perfil-publicacao'>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                    <img src='../assets/perfil.png' alt='Publicacao'/>
+                </div>
+            </div>
         </div>
     )
 }
