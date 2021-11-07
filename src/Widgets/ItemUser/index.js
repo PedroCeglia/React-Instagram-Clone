deff264dbbc281167d79a88999f99621b12dcfb8
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-07 16:20:09 -0300

    Add Config SrcDiretory

--- src/Widgets/ItemUser/index.js
+++ src/Widgets/ItemUser/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 
-export default function ItemUser(){
+export default function ItemUser(props){
+    
+    // Set Image Diretory
+    const [srcDiretory, setSrcDirectory] = useState('')
+    useEffect(()=>{
+        if(props.pathname != null){
+            const listSplit = props.pathname.split('/')
+            let x = 0
+            setSrcDirectory('')
+            while(x <= listSplit.length-2){
+                if(x !== listSplit.lenght-2){
+                    setSrcDirectory( s =>  s +"../" )
+                }
+                x++
+            }        
+        }   
+    },[props.pathname]) 
+
     return(
         <div className='user-item-list'>
             <div className='user-dates'>
-                <img src='assets/perfil.png' alt='User Perfil'/>
+                <img src={srcDiretory + 'assets/perfil.png'} alt='User Perfil'/>
                 <span>user_name</span>
             </div>
         </div>
