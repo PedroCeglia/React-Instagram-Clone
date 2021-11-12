cf5197728e6646bac60abfa94f17072b6c34d4cd
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-12 16:12:03 -0300

    set ItemUser, Add UserPhoto and UserName

--- src/Widgets/ItemUser/index.js
+++ src/Widgets/ItemUser/index.js
@@ -3,27 +3,64 @@ import './style.css'
 
 export default function ItemUser(props){
     
-    // Set Image Diretory
+    // Set User Date
+    const [userName, setUserName] = useState('')
     const [srcDiretory, setSrcDirectory] = useState('')
     useEffect(()=>{
         if(props.pathname != null){
-            const listSplit = props.pathname.split('/')
-            let x = 0
-            setSrcDirectory('')
-            while(x <= listSplit.length-2){
-                if(x !== listSplit.lenght-2){
-                    setSrcDirectory( s =>  s +"../" )
+            if(props.user != null){
+                if(props.user.nome != null){setUserName(props.user.nome)
+                }else{
+                    setUserName('user_name')
                 }
-                x++
-            }        
-        }   
-    },[props.pathname]) 
+                if(props.user.foto != null){
+                    setSrcDirectory(props.user.foto)
+                } else{
+                    const listSplit = props.pathname.split('/')
+                    let x = 0
+                    setSrcDirectory('')
+                    while(x <= listSplit.length-2){
+                        if(x !== listSplit.lenght-2){
+                            setSrcDirectory( s =>  s +"../" )
+                        }
+                        x++
+                    }
+                    setSrcDirectory( s =>  s + 'assets/perfil.png' )    
+                }
+            } else{
+                const listSplit = props.pathname.split('/')
+                let x = 0
+                setSrcDirectory('')
+                while(x <= listSplit.length-2){
+                    if(x !== listSplit.lenght-2){
+                        setSrcDirectory( s =>  s +"../" )
+                    }
+                    x++
+                }
+                setSrcDirectory( s =>  s + 'assets/perfil.png' )
+                setUserName('user_name')                
+            }
+        } else{
+            if(props.user != null){
+                if(props.user.nome != null){setUserName(props.user.nome)
+                }else{
+                    setUserName('user_name')
+                }
+                if(props.user.foto != null){
+                    setSrcDirectory(props.user.foto)
+                } else{ setSrcDirectory('./assets/perfil.png')}
+            } else{
+                setSrcDirectory('./assets/perfil.png')
+                setUserName('user_name')
+            }
+        }     
+    },[props.pathname, props.user]) 
 
     return(
         <div className='user-item-list'>
             <div className='user-dates'>
-                <img src={srcDiretory + 'assets/perfil.png'} alt='User Perfil'/>
-                <span>user_name</span>
+                <img src={srcDiretory} alt='User Perfil'/>
+                <span>{userName}</span>
             </div>
         </div>
     )
