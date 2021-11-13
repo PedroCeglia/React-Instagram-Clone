29104a34dbe62a81305b45365c4d59db9efe825e
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-12 23:07:56 -0300

    Config Feed Posts

--- src/Pages/Home/Widgets/HomeContent/PostContent/ItemPost/index.js
+++ src/Pages/Home/Widgets/HomeContent/PostContent/ItemPost/index.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './style.css'
 
-export default function ItemPost(){
+export default function ItemPost(props){
+    // Get User Post Dates And Midia Date
+    const [userName, setUserName] = useState('user_name')
+    const [userFoto, setUserFoto] = useState('assets/perfil.png')
+    const [postFoto, setPostFoto] = useState('assets/perfil.png')
+    useEffect(()=>{
+        if(props.post != null){
+            if(props.post.fotoUsuario != null){
+                setUserFoto(props.post.fotoUsuario)
+            }
+            setUserName(props.post.nomeUsuario)
+            setPostFoto(props.post.fotoPostagem)
+        } else{
+            setUserName('user_name')
+            setUserFoto('assets/perfil.png')
+            setPostFoto('assets/perfil.png')
+        }
+    },[props.post])
+
     return(
         <div className='post-item'>
             <div className='post-header'>
                 <div className='user-date-post'>
-                <img className='perfil-foto-post' src='assets/perfil.png' alt='User Perfil'/>
-                <span>User</span>
+                <img className='perfil-foto-post' src={userFoto} alt='User Perfil'/>
+                <span>{userName}</span>
                 </div>
                 <img className='menu-post' src='assets/menu.png' alt='Menu Icon'/>
             </div>
             <div className='post-midia'>
-                <img src='assets/perfil.png' alt='Main Post'/>
+                <img src={postFoto} alt='Main Post'/>
             </div>
             <div className='post-icons'>
                 <div className='icons-left'>
