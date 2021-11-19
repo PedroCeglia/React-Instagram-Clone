4a8530c8a70ccbdc6e7026e72e31ac57323313bf
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-19 19:37:53 -0300

    Add Fotos And Videos And OpenMidia Widget

--- src/Pages/Direct/Widgets/DirectContent/MensageDirect/index.js
+++ src/Pages/Direct/Widgets/DirectContent/MensageDirect/index.js
@@ -1,43 +1,22 @@
 import React, {useState} from "react";
 import './style.css'
 
+// Import Widget
+import OpenMidia from './OpenMidia'
+
 export default function MensageDirect(props){
 
-    /*
     const [active, setActive] = useState('open-midia-container')
 
     function handlerImage(){
         setActive("open-midia-container active")
-    }*/
-    return(
-        <p className={props.smsClass}>
-            {props.mensage}
-            <span className='sms-hour'>{props.smsHour}</span>
-        </p>
-    ) 
-    /*
-    if(props.foto != null){
-        if(props.mensage != null && props.mensage != "image"){
+    } 
+    if(props.type == 'foto'){
             return(
                 <div className={props.smsClass}>
-                    <img src={props.foto} alt='image Mensage' onClick={handlerImage}/>
+                    <img src={props.mensage} alt='image Mensage' onClick={handlerImage}/>
                     <OpenMidia 
-                        foto={props.foto}
-                        active={active}
-                        setActiveProps={setActive}
-                    />
-                    <p>                
-                        {props.mensage}
-                        <span className='sms-hour'>{props.smsHour}</span>
-                    </p>
-                </div>
-            )
-        } else{
-            return(
-                <div className={props.smsClass}>
-                    <img src={props.foto} alt='image Mensage' onClick={handlerImage}/>
-                    <OpenMidia 
-                        foto={props.foto}
+                        foto={props.mensage}
                         active={active}
                         setActiveProps={setActive}
                     />
@@ -45,42 +24,24 @@ export default function MensageDirect(props){
                         <span className='sms-hour'>{props.smsHour}</span>
                     </p>
                 </div>
-            )
-        }
-    } else if(props.video != null){
-        if(props.mensage != null && props.mensage != "video"){
-            
-            return(
-                <div className={props.smsClass}>
-                <video controls>
-                    <source src={ props.video } type="video/mp4"/>
-                </video>
-                <p>                
-                    {props.mensage}
-                    <span className='sms-hour'>{props.smsHour}</span>
-                </p>
-                </div>
-            )
-        } else{
-            return(
-                <div className={props.smsClass}>
-                <video width='100%' height='100%' controls>
-                    <source src={ props.video } type="video/mp4"/>
-                </video>
-                <p>                
-                    <span className='sms-hour'>{props.smsHour}</span>
-                </p>
-                </div>
-            )
-        }
+            )        
+    } else if(props.type === 'video'){
+        return(
+            <div className={props.smsClass}>
+            <video width='100%' height='100%' controls>
+                <source src={ props.mensage } type="video/mp4"/>
+            </video>
+            <p>                
+                <span className='sms-hour'>{props.smsHour}</span>
+            </p>
+            </div>
+        )
     } else{
         return(
             <p className={props.smsClass}>
                 {props.mensage}
                 <span className='sms-hour'>{props.smsHour}</span>
             </p>
-        ) 
+        )
     }
-    */
-
 }
